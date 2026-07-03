97a680dd468639a8ae00d2432f1969a3144c4247
Khandaker Ali Ariyan <khandakeraliariyanndc@gmail.com>
2026-07-03 22:40:12 +0600

    refactored the dashboard services

--- backend/src/services/dashboard.service.js
+++ backend/src/services/dashboard.service.js
@@ -1,46 +1,30 @@
 const Device = require("../models/Device");
 const Alert = require("../models/Alert");
+
 const RoomService = require("./room.service");
+const PowerService = require("./power.service");
 
 class DashboardService {
-
     async getDashboardData() {
-
-        const devices = await Device.find().populate("room");
-
-        const alerts = await Alert.find({
-            resolved: false
-        }).sort({ createdAt: -1 });
-
-        const rooms = await RoomService.getAllRooms();
-
-        const totalPower = rooms.reduce(
-            (sum, room) => sum + room.totalPower,
-            0
-        );
-
-        const roomPower = {};
-
-        rooms.forEach((room) => {
-            roomPower[room.name] = room.totalPower;
-        });
+        // Fetch all required data in parallel
+        const [devices, alerts, rooms, power] = await Promise.all([
+            Device.find().populate("room"),
+            Alert.find({ resolved: false }).sort({ createdAt: -1 }),
+            RoomService.getAllRooms(),
+            PowerService.getCurrentPowerUsage(),
+        ]);
 
         return {
-
             devices,
-
+            rooms,
             alerts,
 
-            totalPower,
+            totalPower: power.totalPower,
+            roomPower: power.roomPower,
 
-            roomPower,
-
-            lastUpdated: new Date()
-
+            lastUpdated: new Date(),
         };
-
     }
-
 }
 
 module.exports = new DashboardService();

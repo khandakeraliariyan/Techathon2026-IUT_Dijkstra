6e6078f3d8a093c0620c8a7025d8ed2496d765ce
Khandaker Ali Ariyan <khandakeraliariyanndc@gmail.com>
2026-07-04 02:47:09 +0600

    added some ui and apis

---
+++ frontend/src/pages/Dashboard.jsx
@@ -0,0 +1,17 @@
+const Dashboard = () => {
+    return (
+        <div className="p-8">
+
+            <h1 className="text-4xl font-bold">
+                Smart Office Dashboard
+            </h1>
+
+            <p className="mt-2 text-slate-400">
+                Real-time Energy Monitoring System
+            </p>
+
+        </div>
+    );
+};
+
+export default Dashboard;
